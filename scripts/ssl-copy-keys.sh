5e8808bd7c93ee1e3521c86450c0191b747ad94e
Benjamin Borbe <bborbe@rocketnews.de>
2016-06-11 18:49:00 +0200

    test

--- scripts/ssl-copy-keys.sh
+++ scripts/ssl-copy-keys.sh
@@ -7,60 +7,38 @@ set -o errtrace
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
 
-
+# kubernetes-master
 mkdir -p ${SCRIPT_ROOT}/../kubernetes-master/ssl
-cp ${SCRIPT_ROOT}/ca.pem ${SCRIPT_ROOT}/../kubernetes-master/ssl/ca.pem
-cp ${SCRIPT_ROOT}/kubernetes-master.pem ${SCRIPT_ROOT}/../kubernetes-master/ssl/node.pem
-cp ${SCRIPT_ROOT}/kubernetes-master-key.pem ${SCRIPT_ROOT}/../kubernetes-master/ssl/node-key.pem
+cp ${SCRIPT_ROOT}/kubernetes-ca.pem ${SCRIPT_ROOT}/../kubernetes-master/ssl/ca.pem
+cp ${SCRIPT_ROOT}/kubernetes-apiserver.pem ${SCRIPT_ROOT}/../kubernetes-master/ssl/node.pem
+cp ${SCRIPT_ROOT}/kubernetes-apiserver-key.pem ${SCRIPT_ROOT}/../kubernetes-master/ssl/node-key.pem
 #chmod 600 ${SCRIPT_ROOT}/../kubernetes-master/ssl/*.pem
 chown root:root ${SCRIPT_ROOT}/../kubernetes-master/ssl/*.pem
 
+# kubernetes-storage
 mkdir -p ${SCRIPT_ROOT}/../kubernetes-storage/ssl
-cp ${SCRIPT_ROOT}/ca.pem ${SCRIPT_ROOT}/../kubernetes-storage/ssl/ca.pem
+cp ${SCRIPT_ROOT}/kubernetes-ca.pem ${SCRIPT_ROOT}/../kubernetes-storage/ssl/ca.pem
 cp ${SCRIPT_ROOT}/kubernetes-storage.pem ${SCRIPT_ROOT}/../kubernetes-storage/ssl/node.pem
 cp ${SCRIPT_ROOT}/kubernetes-storage-key.pem ${SCRIPT_ROOT}/../kubernetes-storage/ssl/node-key.pem
 #chmod 600 ${SCRIPT_ROOT}/../kubernetes-storage/ssl/*.pem
 chown root:root ${SCRIPT_ROOT}/../kubernetes-storage/ssl/*.pem
 
-mkdir -p ${SCRIPT_ROOT}/../kubernetes-etcd0/ssl
-cp ${SCRIPT_ROOT}/ca.pem ${SCRIPT_ROOT}/../kubernetes-etcd0/ssl/ca.pem
-cp ${SCRIPT_ROOT}/kubernetes-etcd0.pem ${SCRIPT_ROOT}/../kubernetes-etcd0/ssl/node.pem
-cp ${SCRIPT_ROOT}/kubernetes-etcd0-key.pem ${SCRIPT_ROOT}/../kubernetes-etcd0/ssl/node-key.pem
-#chmod 600 ${SCRIPT_ROOT}/../kubernetes-etcd0/ssl/*.pem
-chown root:root ${SCRIPT_ROOT}/../kubernetes-etcd0/ssl/*.pem
+# kubernetes-etcd
+for ((i=0; i < 3; i++)) do
+	mkdir -p ${SCRIPT_ROOT}/../kubernetes-etcd${i}/ssl
+	cp ${SCRIPT_ROOT}/kubernetes-ca.pem ${SCRIPT_ROOT}/../kubernetes-etcd${i}/ssl/ca.pem
+	cp ${SCRIPT_ROOT}/kubernetes-etcd${i}.pem ${SCRIPT_ROOT}/../kubernetes-etcd${i}/ssl/node.pem
+	cp ${SCRIPT_ROOT}/kubernetes-etcd${i}-key.pem ${SCRIPT_ROOT}/../kubernetes-etcd${i}/ssl/node-key.pem
+	#chmod 600 ${SCRIPT_ROOT}/../kubernetes-etcd${i}/ssl/*.pem
+	chown root:root ${SCRIPT_ROOT}/../kubernetes-etcd${i}/ssl/*.pem
+done
 
-mkdir -p ${SCRIPT_ROOT}/../kubernetes-etcd1/ssl
-cp ${SCRIPT_ROOT}/ca.pem ${SCRIPT_ROOT}/../kubernetes-etcd1/ssl/ca.pem
-cp ${SCRIPT_ROOT}/kubernetes-etcd1.pem ${SCRIPT_ROOT}/../kubernetes-etcd1/ssl/node.pem
-cp ${SCRIPT_ROOT}/kubernetes-etcd1-key.pem ${SCRIPT_ROOT}/../kubernetes-etcd1/ssl/node-key.pem
-#chmod 600 ${SCRIPT_ROOT}/../kubernetes-etcd1/ssl/*.pem
-chown root:root ${SCRIPT_ROOT}/../kubernetes-etcd1/ssl/*.pem
-
-mkdir -p ${SCRIPT_ROOT}/../kubernetes-etcd2/ssl
-cp ${SCRIPT_ROOT}/ca.pem ${SCRIPT_ROOT}/../kubernetes-etcd2/ssl/ca.pem
-cp ${SCRIPT_ROOT}/kubernetes-etcd2.pem ${SCRIPT_ROOT}/../kubernetes-etcd2/ssl/node.pem
-cp ${SCRIPT_ROOT}/kubernetes-etcd2-key.pem ${SCRIPT_ROOT}/../kubernetes-etcd2/ssl/node-key.pem
-#chmod 600 ${SCRIPT_ROOT}/../kubernetes-etcd2/ssl/*.pem
-chown root:root ${SCRIPT_ROOT}/../kubernetes-etcd2/ssl/*.pem
-
-mkdir -p ${SCRIPT_ROOT}/../kubernetes-worker0/ssl
-cp ${SCRIPT_ROOT}/ca.pem ${SCRIPT_ROOT}/../kubernetes-worker0/ssl/ca.pem
-cp ${SCRIPT_ROOT}/kubernetes-worker0.pem ${SCRIPT_ROOT}/../kubernetes-worker0/ssl/node.pem
-cp ${SCRIPT_ROOT}/kubernetes-worker0-key.pem ${SCRIPT_ROOT}/../kubernetes-worker0/ssl/node-key.pem
-#chmod 600 ${SCRIPT_ROOT}/../kubernetes-worker0/ssl/*.pem
-chown root:root ${SCRIPT_ROOT}/../kubernetes-worker0/ssl/*.pem
-
-mkdir -p ${SCRIPT_ROOT}/../kubernetes-worker1/ssl
-cp ${SCRIPT_ROOT}/ca.pem ${SCRIPT_ROOT}/../kubernetes-worker1/ssl/ca.pem
-cp ${SCRIPT_ROOT}/kubernetes-worker1.pem ${SCRIPT_ROOT}/../kubernetes-worker1/ssl/node.pem
-cp ${SCRIPT_ROOT}/kubernetes-worker1-key.pem ${SCRIPT_ROOT}/../kubernetes-worker1/ssl/node-key.pem
-#chmod 600 ${SCRIPT_ROOT}/../kubernetes-worker1/ssl/*.pem
-chown root:root ${SCRIPT_ROOT}/../kubernetes-worker1/ssl/*.pem
-
-mkdir -p ${SCRIPT_ROOT}/../kubernetes-worker2/ssl
-cp ${SCRIPT_ROOT}/ca.pem ${SCRIPT_ROOT}/../kubernetes-worker2/ssl/ca.pem
-cp ${SCRIPT_ROOT}/kubernetes-worker2.pem ${SCRIPT_ROOT}/../kubernetes-worker2/ssl/node.pem
-cp ${SCRIPT_ROOT}/kubernetes-worker2-key.pem ${SCRIPT_ROOT}/../kubernetes-worker2/ssl/node-key.pem
-#chmod 600 ${SCRIPT_ROOT}/../kubernetes-worker2/ssl/*.pem
-chown root:root ${SCRIPT_ROOT}/../kubernetes-worker2/ssl/*.pem
-
+# kubernetes-worker
+for ((i=0; i < 3; i++)) do
+	mkdir -p ${SCRIPT_ROOT}/../kubernetes-worker${i}/ssl
+	cp ${SCRIPT_ROOT}/kubernetes-ca.pem ${SCRIPT_ROOT}/../kubernetes-worker${i}/ssl/ca.pem
+	cp ${SCRIPT_ROOT}/kubernetes-worker${i}.pem ${SCRIPT_ROOT}/../kubernetes-worker${i}/ssl/node.pem
+	cp ${SCRIPT_ROOT}/kubernetes-worker${i}-key.pem ${SCRIPT_ROOT}/../kubernetes-worker${i}/ssl/node-key.pem
+	#chmod 600 ${SCRIPT_ROOT}/../kubernetes-worker${i}/ssl/*.pem
+	chown root:root ${SCRIPT_ROOT}/../kubernetes-worker${i}/ssl/*.pem
+done
