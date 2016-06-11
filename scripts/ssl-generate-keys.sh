1ff436ca813608d9f01f370fce9aa0ad3ddf150a
Benjamin Borbe <bborbe@rocketnews.de>
2016-06-11 16:37:25 +0200

    update cn

--- scripts/ssl-generate-keys.sh
+++ scripts/ssl-generate-keys.sh
@@ -13,16 +13,12 @@ SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
 openssl genrsa -out ${SCRIPT_ROOT}/ca-key.pem 2048
 openssl req -x509 -new -nodes -key ${SCRIPT_ROOT}/ca-key.pem -days 10000 -out ${SCRIPT_ROOT}/ca.pem -subj "/CN=kube-ca"
 
-# Admin Key
-openssl genrsa -out ${SCRIPT_ROOT}/admin-key.pem 2048
-openssl req -new -key ${SCRIPT_ROOT}/admin-key.pem -out ${SCRIPT_ROOT}/admin.csr -subj "/CN=kube-admin"
-openssl x509 -req -in ${SCRIPT_ROOT}/admin.csr -CA ${SCRIPT_ROOT}/ca.pem -CAkey ${SCRIPT_ROOT}/ca-key.pem -CAcreateserial -out ${SCRIPT_ROOT}/admin.pem -days 365
-
 
 # kubernetes-master
 openssl genrsa -out ${SCRIPT_ROOT}/kubernetes-master-key.pem 2048
 KUBERNETES_SVC=10.103.0.1 FIREWALL_IP=172.16.60.6 MASTER_IP=172.16.20.10 openssl req -new -key ${SCRIPT_ROOT}/kubernetes-master-key.pem -out ${SCRIPT_ROOT}/kubernetes-master.csr -subj "/CN=kubernetes-master" -config ${SCRIPT_ROOT}/master-openssl.cnf
 KUBERNETES_SVC=10.103.0.1 FIREWALL_IP=172.16.60.6 MASTER_IP=172.16.20.10 openssl x509 -req -in ${SCRIPT_ROOT}/kubernetes-master.csr -CA ${SCRIPT_ROOT}/ca.pem -CAkey ${SCRIPT_ROOT}/ca-key.pem -CAcreateserial -out ${SCRIPT_ROOT}/kubernetes-master.pem -days 365 -extensions v3_req -extfile ${SCRIPT_ROOT}/master-openssl.cnf
+
 
 
 # kubernetes-storage
@@ -59,4 +55,10 @@ NODE_IP=172.16.20.16 openssl x509 -req -in ${SCRIPT_ROOT}/kubernetes-worker1.csr
 openssl genrsa -out ${SCRIPT_ROOT}/kubernetes-worker2-key.pem 2048
 NODE_IP=172.16.20.17 openssl req -new -key ${SCRIPT_ROOT}/kubernetes-worker2-key.pem -out ${SCRIPT_ROOT}/kubernetes-worker2.csr -subj "/CN=kubernetes-worker2" -config ${SCRIPT_ROOT}/node-openssl.cnf
 NODE_IP=172.16.20.17 openssl x509 -req -in ${SCRIPT_ROOT}/kubernetes-worker2.csr -CA ${SCRIPT_ROOT}/ca.pem -CAkey ${SCRIPT_ROOT}/ca-key.pem -CAcreateserial -out ${SCRIPT_ROOT}/kubernetes-worker2.pem -days 365 -extensions v3_req -extfile ${SCRIPT_ROOT}/node-openssl.cnf
+
+
+# Admin Key
+openssl genrsa -out ${SCRIPT_ROOT}/admin-key.pem 2048
+openssl req -new -key ${SCRIPT_ROOT}/admin-key.pem -out ${SCRIPT_ROOT}/admin.csr -subj "/CN=kube-admin"
+openssl x509 -req -in ${SCRIPT_ROOT}/admin.csr -CA ${SCRIPT_ROOT}/ca.pem -CAkey ${SCRIPT_ROOT}/ca-key.pem -CAcreateserial -out ${SCRIPT_ROOT}/admin.pem -days 365
 
